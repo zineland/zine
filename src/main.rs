0a176807fbd565ea88f2d76148780d8e49fa3ebf
Folyd <lyshuhow@gmail.com>
2022-02-28 22:27:30 +0800

    Auto watch templates change

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
+use std::{env, sync::mpsc, time::Duration};
+
 use anyhow::Result;
 
+use notify::Watcher;
 use zine::{Builder, Parser};
 
 fn main() -> Result<()> {
+    if matches!(env::args().nth(1).as_deref(), Some("-w" | "--watch")) {
+        println!("Watching...");
+
+        let (tx, rx) = mpsc::channel();
+        let mut watcher = notify::watcher(tx, Duration::from_secs(1))?;
+        watcher.watch("templates", notify::RecursiveMode::Recursive)?;
+
+        loop {
+            match rx.recv() {
+                Ok(_) => build()?,
+                Err(err) => println!("watch error: {:?}", &err),
+            }
+        }
+    } else {
+        build()?;
+    }
+
+    Ok(())
+}
+
+fn build() -> Result<()> {
     let zine = Parser::new("demo");
     let site = zine.parse()?;
     println!("{:?}", site);
     let builder = Builder::new("dist")?;
-    builder.build(site)?;
-    Ok(())
+    builder.build(site)
 }
