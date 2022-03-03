7e88c3c099f99379f209a452e42fcefb20a7e39e
Folyd <lyshuhow@gmail.com>
2022-03-03 14:39:24 +0800

    Customize tailwind typography styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,39 @@
 //
 // tailwindcss -o target/zine.css --watch --minify
 module.exports = {
-    content: [
-        './templates/**/*.jinja',
-    ],
-    theme: {
-        extend: {},
+  content: [
+    './templates/**/*.jinja',
+  ],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: 'var(--primary-color)',
+              textDecoration: 'none',
+              fontWeight: '400',
+            },
+            'a:hover': {
+              textDecoration: 'underline',
+            },
+            strong: {
+              fontWeight: '500',
+            },
+            blockquote: {
+              color: "#7c8088",
+              // Make font weight and style to normal
+              fontWeight: '400',
+              fontStyle: 'normal',
+              // Disable blockquote's quotes style
+              quotes: 'none',
+            },
+          },
+        },
+      },
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
