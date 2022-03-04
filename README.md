3407a4db4567468b6cf3fb94282eca8584668dd7
Folyd <lyshuhow@gmail.com>
2022-03-04 16:03:51 +0800

    Have a integration with clap

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ Zine - an simple and opinionated tool to build your own magazine.
 ## TODO
 
 [] Support table of content
+
 [] Support code syntax highlight
+
 [] Support render OGP meta
+
+[] Support RSS Feed
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,76 @@
-use std::{env, fs, path::PathBuf, sync::mpsc, time::Duration};
+use std::{fs, path::PathBuf, sync::mpsc, time::Duration};
 
 use anyhow::Result;
-
+use clap::StructOpt;
 use notify::Watcher;
 use zine::{Builder, Parser};
 
+#[derive(Debug, clap::Parser)]
+#[clap(name = "zine")]
+#[clap(about = "An simple and opinionated tool to build your own magazine.", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, clap::Subcommand)]
+enum Commands {
+    /// Build zine site.
+    #[clap(arg_required_else_help = true)]
+    Build {
+        /// The source directory of zine site.
+        source: String,
+        /// The destination directory. Default dest dir is `build`.
+        dest: Option<String>,
+        /// Enable watching.
+        #[clap(short = 'w', long = "watch")]
+        watch: bool,
+    },
+    Serve,
+}
+
 fn main() -> Result<()> {
-    build()?;
+    match Cli::parse().command {
+        Commands::Build {
+            source,
+            dest,
+            watch,
+        } => {
+            let dest = dest.unwrap_or_else(|| "build".into());
+            build(&source, &dest)?;
 
-    if matches!(env::args().nth(1).as_deref(), Some("-w" | "--watch")) {
-        println!("Watching...");
+            if watch {
+                println!("Watching...");
+                let (tx, rx) = mpsc::channel();
+                let mut watcher = notify::watcher(tx, Duration::from_secs(1))?;
+                watcher.watch("templates", notify::RecursiveMode::Recursive)?;
 
-        let (tx, rx) = mpsc::channel();
-        let mut watcher = notify::watcher(tx, Duration::from_secs(1))?;
-        watcher.watch("templates", notify::RecursiveMode::Recursive)?;
-
-        loop {
-            match rx.recv() {
-                Ok(_) => build()?,
-                Err(err) => println!("watch error: {:?}", &err),
+                loop {
+                    match rx.recv() {
+                        Ok(_) => build(&source, &dest)?,
+                        Err(err) => println!("watch error: {:?}", &err),
+                    }
+                }
             }
         }
+        Commands::Serve => {}
     }
 
     Ok(())
 }
 
-fn build() -> Result<()> {
-    let site = Parser::new("demo").parse()?;
+fn build(source: &str, dest: &str) -> Result<()> {
+    let site = Parser::new(source).parse()?;
     println!("{:?}", site);
-    Builder::new("dist")?.build(site)?;
-    fs::copy("target/zine.css", "dist/zine.css").expect("File target/zine.css doesn't exists");
-    copy_static_assets("demo", "dist")?;
+    Builder::new(dest)?.build(site)?;
+    fs::copy("target/zine.css", format!("{}/zine.css", dest))
+        .expect("File target/zine.css doesn't exists");
+    copy_static_assets(source, dest)?;
     Ok(())
 }
 
-fn copy_static_assets(source: &str, dist: &str) -> Result<()> {
-    let dist = PathBuf::from(dist);
+fn copy_static_assets(source: &str, dest: &str) -> Result<()> {
+    let dist = PathBuf::from(dest);
     for entry in walkdir::WalkDir::new(&format!("{}/static", source)) {
         let entry = entry?;
         let path = entry.path();
