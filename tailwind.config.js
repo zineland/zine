0d4e4f8415c70582e1130bb7a38eb53c21ffa8d0
Folyd <lyshuhow@gmail.com>
2022-03-05 22:48:24 +0800

    Add tailwind line-clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,43 +3,46 @@
 //
 // tailwindcss -o target/zine.css --watch --minify
 module.exports = {
-  content: [
-    './templates/**/*.jinja',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: 'var(--primary-color)',
-        secondary: 'var(--secondary-color)',
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            a: {
-              color: 'var(--primary-link-color)',
-              textDecoration: 'none',
-              fontWeight: '400',
+    content: [
+        './templates/**/*.jinja',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: 'var(--primary-color)',
+                secondary: 'var(--secondary-color)',
             },
-            'a:hover': {
-              textDecoration: 'underline',
+            typography: {
+                DEFAULT: {
+                    css: {
+                        a: {
+                            color: 'var(--primary-link-color)',
+                            textDecoration: 'none',
+                            fontWeight: '400',
+                        },
+                        'a:hover': {
+                            textDecoration: 'underline',
+                        },
+                        strong: {
+                            fontWeight: '500',
+                        },
+                        blockquote: {
+                            color: "#7c8088",
+                            // Make font weight and style to normal
+                            fontWeight: '400',
+                            fontStyle: 'normal',
+                            // Disable blockquote's quotes style
+                            quotes: 'none',
+                        },
+                    },
+                },
             },
-            strong: {
-              fontWeight: '500',
-            },
-            blockquote: {
-              color: "#7c8088",
-              // Make font weight and style to normal
-              fontWeight: '400',
-              fontStyle: 'normal',
-              // Disable blockquote's quotes style
-              quotes: 'none',
-            },
-          },
         },
-      },
     },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+    plugins: [
+        // A plugin to pretty markdown content.
+        require('@tailwindcss/typography'),
+        // A plugin to truncate text to a fixed number of lines.
+        require('@tailwindcss/line-clamp'),
+    ],
 }
