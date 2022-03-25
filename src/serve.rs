bc79a16cca579f11a03eb4142d86e5641e2b01f9
Folyd <lyshuhow@gmail.com>
2022-03-25 17:25:57 +0800

    Serve command: handling files not found (#10)

--- src/serve.rs
+++ src/serve.rs
@@ -1,8 +1,11 @@
-use std::{env, net::SocketAddr, path::Path};
+use std::{env, io, net::SocketAddr, path::Path};
 
 use crate::{build::watch_build, TEMP_ZINE_BUILD_DIR};
 use anyhow::Result;
-use tower_http::services::ServeDir;
+use http_body::Full;
+use hyper::{body::HttpBody, Response, StatusCode};
+use tower::ServiceBuilder;
+use tower_http::services::{fs::ServeFileSystemResponseBody, ServeDir};
 
 static ZINE_BANNER: &str = r"
 
@@ -18,7 +21,26 @@ static ZINE_BANNER: &str = r"
 pub async fn run_serve(source: String, port: u16) -> Result<()> {
     let tmp_dir = env::temp_dir().join(TEMP_ZINE_BUILD_DIR);
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    let service = ServeDir::new(&tmp_dir);
+    let service = ServiceBuilder::new()
+        .and_then(
+            |response: Response<ServeFileSystemResponseBody>| async move {
+                let response = if response.status() == StatusCode::NOT_FOUND {
+                    let body = Full::from("404 Not Found")
+                        .map_err(|err| match err {})
+                        .boxed();
+                    Response::builder()
+                        .status(StatusCode::NOT_FOUND)
+                        .body(body)
+                        .unwrap()
+                } else {
+                    response.map(|body| body.boxed())
+                };
+
+                Ok::<_, io::Error>(response)
+            },
+        )
+        .service(ServeDir::new(&tmp_dir));
+
     tokio::spawn(async move {
         watch_build(Path::new(&source), tmp_dir.as_path(), true)
             .await
