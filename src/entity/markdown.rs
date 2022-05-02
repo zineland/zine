9020c199ffd9bb50e1f4da4eb0e26f1141d255fb
Folyd <lyshuhow@gmail.com>
2022-05-02 15:38:43 +0800

    Set default highlight theme

--- src/entity/markdown.rs
+++ src/entity/markdown.rs
@@ -3,19 +3,15 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all(deserialize = "snake_case"))]
 pub struct MarkdownConfig {
-<<<<<<< HEAD
     #[serde(default = "MarkdownConfig::default_highlight_code")]
     pub highlight_code: bool,
     #[serde(default = "MarkdownConfig::default_highlight_theme")]
-=======
->>>>>>> Support customize highlight theme
     pub highlight_theme: String,
 }
 
 impl Default for MarkdownConfig {
     fn default() -> Self {
         Self {
-<<<<<<< HEAD
             highlight_code: true,
             highlight_theme: Self::default_highlight_theme(),
         }
@@ -33,9 +29,3 @@ impl MarkdownConfig {
         true
     }
 }
-=======
-            highlight_theme: String::from("monokai"),
-        }
-    }
-}
->>>>>>> Support customize highlight theme
