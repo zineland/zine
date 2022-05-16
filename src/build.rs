57df6cfe95b89d46ad36022e8a4299d0148a15a5
Folyd <lyshuhow@gmail.com>
2022-05-16 22:03:24 +0800

    Spawn the build process as a blocking task, avoid starving other tasks. (#64)

--- src/build.rs
+++ src/build.rs
@@ -5,47 +5,54 @@ use anyhow::Result;
 use notify::Watcher;
 
 pub async fn watch_build<P: AsRef<Path>>(source: P, dest: P, watch: bool) -> Result<()> {
-    if let Err(err) = _watch_build(source.as_ref(), dest.as_ref(), watch) {
+    data::load(&source);
+
+    let source_path = source.as_ref().to_path_buf();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        // Save zine data only when the process gonna exist
+        data::export(source_path).unwrap();
+        std::process::exit(0);
+    });
+
+    if let Err(err) = _watch_build(source.as_ref(), dest.as_ref(), watch).await {
         println!("Error: {}", &err);
         std::process::exit(1);
     }
     Ok(())
 }
 
-fn _watch_build(source: &Path, dest: &Path, watch: bool) -> Result<()> {
-    data::load(&source);
+async fn _watch_build(source: &Path, dest: &Path, watch: bool) -> Result<()> {
+    let source = source.to_path_buf();
+    let dest = dest.to_path_buf();
 
-    build(source, dest)?;
+    // Spawn the build process as a blocking task, avoid starving other tasks.
+    tokio::task::spawn_blocking(move || {
+        build(&source, &dest)?;
 
-    let source_path_buf = source.to_path_buf();
-    tokio::spawn(async move {
-        tokio::signal::ctrl_c().await.unwrap();
-        // Save zine data only when the process gonna exist
-        data::export(source_path_buf).unwrap();
-        std::process::exit(0);
-    });
+        if watch {
+            println!("Watching...");
+            let (tx, rx) = mpsc::channel();
+            let mut watcher = notify::watcher(tx, Duration::from_secs(1))?;
+            watcher.watch(&source, notify::RecursiveMode::Recursive)?;
 
-    if watch {
-        println!("Watching...");
-        let (tx, rx) = mpsc::channel();
-        let mut watcher = notify::watcher(tx, Duration::from_secs(1))?;
-        watcher.watch(&source, notify::RecursiveMode::Recursive)?;
+            // Watch zine's templates and static directory in debug mode to support reload.
+            #[cfg(debug_assertions)]
+            {
+                watcher.watch("templates", notify::RecursiveMode::Recursive)?;
+                watcher.watch("static", notify::RecursiveMode::Recursive)?;
+            }
 
-        // Watch zine's templates and static directory in debug mode to support reload.
-        #[cfg(debug_assertions)]
-        {
-            watcher.watch("templates", notify::RecursiveMode::Recursive)?;
-            watcher.watch("static", notify::RecursiveMode::Recursive)?;
-        }
-
-        loop {
-            match rx.recv() {
-                Ok(_) => build(source, dest)?,
-                Err(err) => println!("watch error: {:?}", &err),
+            loop {
+                match rx.recv() {
+                    Ok(_) => build(&source, &dest)?,
+                    Err(err) => println!("watch error: {:?}", &err),
+                }
             }
         }
-    }
-    Ok(())
+        anyhow::Ok(())
+    })
+    .await?
 }
 
 fn build(source: &Path, dest: &Path) -> Result<()> {
