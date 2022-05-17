f072ed8a08ef7d1367dfbaed702d0d488ed22a9c
Folyd <lyshuhow@gmail.com>
2022-05-17 12:27:16 +0800

    Add OGP meta for author list page

--- src/entity/author.rs
+++ src/entity/author.rs
@@ -1,5 +1,6 @@
 use std::{borrow::Cow, path::Path};
 
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use tera::Context;
 
@@ -44,6 +45,10 @@ impl<'a> AuthorList<'a> {
             article_count,
         });
     }
+
+    fn render_title(&self) -> Result<String> {
+        engine::render_str(r#"{{ fluent(key="author-list") }}"#, &Context::new())
+    }
 }
 
 impl Entity for Author {
@@ -76,7 +81,15 @@ impl Entity for Author {
 
 impl<'a> Entity for AuthorList<'a> {
     fn render(&self, mut context: Context, dest: &Path) -> anyhow::Result<()> {
-        // TODO: open graph
+        context.insert(
+            "meta",
+            &Meta {
+                title: Cow::Owned(self.render_title()?),
+                description: Cow::Owned(String::new()),
+                url: Some(Cow::Borrowed("authors")),
+                image: None,
+            },
+        );
         context.insert("authors", &self.authors);
         engine::render("author-list.jinja", &context, dest.join("authors"))?;
         Ok(())
