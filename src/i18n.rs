0d53f30c2e44de12848992b1a4e65ad23f2f96e8
Folyd <lyshuhow@gmail.com>
2022-11-19 15:05:48 +0800

    Feat locale

--- src/i18n.rs
+++ src/i18n.rs
@@ -1,21 +1,14 @@
 use std::collections::HashMap;
 
-static LOCALE_ENGLISH: (&str, &str) = ("en", "English");
-static LOCALE_FRANCE: (&str, &str) = ("fr", "Français");
-static LOCALE_SPANISH: (&str, &str) = ("es", "Español");
-static LOCALE_JAPANESE: (&str, &str) = ("ja", "日本語");
-static LOCALE_SIMPLIFIED_CHINESE: (&str, &str) = ("zh_CN", "简体中文");
-static LOCALE_TRADITIONAL_CHINESE: (&str, &str) = ("zh_TW", "繁體中文");
+static LOCALES: [(&str, &str); 6] = [
+    ("en", "English"),
+    ("fr", "Français"),
+    ("es", "Español"),
+    ("ja", "日本語"),
+    ("zh_CN", "简体中文"),
+    ("zh_TW", "繁體中文"),
+];
 
 pub fn get_locale_name(locale: &str) -> Option<&'static str> {
-    HashMap::from([
-        LOCALE_ENGLISH,
-        LOCALE_FRANCE,
-        LOCALE_SPANISH,
-        LOCALE_JAPANESE,
-        LOCALE_SIMPLIFIED_CHINESE,
-        LOCALE_TRADITIONAL_CHINESE,
-    ])
-    .get(locale)
-    .copied()
+    HashMap::from(LOCALES).get(locale).copied()
 }
