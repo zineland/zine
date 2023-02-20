799cb7624b14beb6023f96362a6d111b9c652bfe
Adam.S <Bas-Man@users.noreply.github.com>
2023-02-20 09:24:20 +0900

    Changes to entity/sites.rs

--- src/entity/site.rs
+++ src/entity/site.rs
@@ -1,6 +1,9 @@
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
+use std::io::prelude::*;
+use std::path::Path;
 
-#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Site {
     /// The absolute url of this site.
     pub url: String,
@@ -18,6 +21,38 @@ pub struct Site {
     #[serde(rename(deserialize = "menu"))]
     #[serde(default)]
     pub menus: Vec<Menu>,
+}
+
+impl Default for Site {
+    fn default() -> Self {
+        Self {
+            url: "http://localhost".into(),
+            cdn: None,
+            name: "My New Magazine Powered by Rust!".into(),
+            description: None,
+            edit_url: None,
+            social_image: None,
+            locale: "en".into(),
+            menus: vec![],
+        }
+    }
+}
+
+impl Site {
+    /// Writes the site TOML file in the root of the Zine magazine.
+    /// This should only be called when creating a new Zine magazine
+    pub(crate) fn write_toml(&self, path: &Path) -> Result<()> {
+        let mut file = std::fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(path)?;
+
+        let toml_str = toml::to_string(&self)?;
+
+        file.write_all("\n[site]\n".as_bytes())?;
+        file.write_all(toml_str.as_bytes())?;
+        Ok(())
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
